310b01d3c6c147913364ed205e26de7a68a462ec
Brandon Colbert <expertdash@gmail.com>
2019-05-08 17:50:07 -0400

    0.1.0
    -Uploaded code
    -Lan and WiFi semi-working (detection through UDP broadcast and handshake)
    -Adding UI

---
+++ RemAL/RemAL/LanManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemAL {
+	class LanManager {
+		private const int DELAY_TIME = 100;
+		private static bool isRunning;
+		private static UdpClient listener;
+
+		public static void Start(int port) {
+			isRunning = true;
+
+			if(listener == null) {
+				new Task(async () => {
+					listener = new UdpClient(port);
+					listener.EnableBroadcast = true;
+
+					Debug.WriteLine("Listening on " + (listener.Client.LocalEndPoint as IPEndPoint).Address);
+
+					while(isRunning) {
+						var data = await listener.ReceiveAsync();
+
+						if(isRunning) {
+							var request = Encoding.ASCII.GetString(data.Buffer);
+							Socket client = listener.Client;
+
+							client.Connect(data.RemoteEndPoint);
+
+							Debug.WriteLine("Connected client");
+
+							if(request == "REMAL_DETECT") {
+								Debug.WriteLine("Sending detection");
+
+								client.Send(Encoding.ASCII.GetBytes("REMAL_DETECT"));
+							} else {
+								bool didHandshake = false, keepAlive = true;
+
+								Debug.WriteLine("Waiting for handshake");
+
+								new Task(() => {
+									while(isRunning && keepAlive) {
+										byte[] b = new byte[64];
+
+										int length = client.Receive(b);
+										onMessage(Encoding.ASCII.GetString(b, 0, length), ref didHandshake);
+									}
+
+									client.Shutdown(SocketShutdown.Both);
+									client.Dispose();
+								}).Start();
+
+								new Task(async () => {
+									await Task.Delay(5000);
+
+									if(!didHandshake) {
+										keepAlive = false;
+										Debug.WriteLine("Terminating for being alive too long");
+									}
+								}).Start();
+
+								Debug.WriteLine(request);
+							}
+						}
+					}
+
+					listener.Dispose();
+					listener = null;
+				}).Start();
+			}
+		}
+
+		public static void Stop() {
+			isRunning = false;
+		}
+
+		private static void onMessage(string msg, ref bool didHandshake) {
+			Debug.WriteLine("[RemAL] " + msg);
+		}
+	}
+}
---
+++ RemAL/RemAL/MainPage.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking;
+using Windows.Networking.Connectivity;
+using Windows.Networking.Sockets;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+using System.Text;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RemAL {
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page {
+		private static TextBlock outputBlock;
+
+		public MainPage() {
+			InitializeComponent();
+
+			outputBlock = outputView;
+        }
+
+		private void DragOver_TileCreate(object sender, DragEventArgs e) {
+			e.AcceptedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Link;
+			e.DragUIOverride.Caption = "Create tile for app";
+		}
+
+		private async void Drop_TileCreate(object sender, DragEventArgs e) {
+			if(e.DataView.Contains(StandardDataFormats.StorageItems)) {
+				var items = await e.DataView.GetStorageItemsAsync();
+
+				if(items.Count > 0)
+					createTile(items[0].Path);
+			}
+		}
+
+		private void createTile(String path) {
+			outputText("Create tile for: " + path);
+		}
+
+		public static void outputText(Object text) {
+			if(outputBlock != null)
+				outputBlock.Text = outputBlock.Text + "\n" + text.ToString();
+		}
+
+		private void Click_Con_USB(object sender, RoutedEventArgs e) {
+			if(checkboxWiFi.IsChecked.Value) {
+				
+			} else {
+				
+			}
+		}
+
+		private async void Click_Con_WiFi(object sender, RoutedEventArgs e) {
+			if(checkboxUSB.IsChecked.Value) {
+				
+			} else {
+				var listener = new TcpListener(IPAddress.Any, 24545);
+
+				outputText("Started listener on " + (listener.LocalEndpoint as IPEndPoint).Address.ToString());
+
+				listener.Start();
+
+				outputText("Accepting clients");
+
+				TcpClient client = await listener.AcceptTcpClientAsync();
+
+				outputText("Got connection from: " + (client.Client.RemoteEndPoint as IPEndPoint).Address);
+
+				byte[] buffer = new byte[64];
+				var data = await client.GetStream().ReadAsync(buffer, 0, buffer.Length);
+
+				outputText(Encoding.ASCII.GetString(buffer));
+
+			}
+		}
+
+		private async void Click_Con_LAN(object sender, RoutedEventArgs e) {
+			if(checkboxLAN.IsChecked.Value) {
+				LanManager.Start(24545);
+			} else {
+				LanManager.Stop();
+			}
+		}
+
+		private void Click_Con_Bluetooth(object sender, RoutedEventArgs e) {
+			if(checkboxBluetooth.IsChecked.Value) {
+
+			} else {
+
+			}
+		}
+
+		private string GetLocalAddress() {
+			return NetworkInformation.GetHostNames().FirstOrDefault(hostname => hostname.IPInformation != null && hostname.Type == HostNameType.Ipv4).ToString();
+		}
+	}
+}
